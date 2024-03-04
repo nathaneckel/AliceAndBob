e4063be7cee6237cf3d73fecc8f5413b76018e05
nathaneckel <nathan@patientpaperwork.com>
2024-03-04 11:48:22 -0500

    I went further in IntelliJ for now

---
+++ AliceAndBob.java
@@ -0,0 +1,30 @@
+public class AliceAndBob {
+    import java.util.Scanner;
+
+// It's been so long since I used Replit that I am not yet overcoming being stuck
+// I started this IntelliJ project in the hopes of being able to progress further
+// It worked, this is progress however I'm not overcoming the errors yet
+// The fixes are telling me to refresh the IntelliJ caches
+// I don't want to risk anything that could mess up my other IntelliJ projects
+// So for now I will move to the next lab and ideally back to Quiz1.
+
+    public class AliceAndBob {
+
+        Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+    System.out.println("Hello, " + name + "!");
+//
+//if name = "Alice"
+//            System.out.println("Hello, Alice!");
+//
+//  else
+//          if name = "Bob"
+//            System.out.println("Hello, Bob!");
+//
+//    else System.out.printIn("Hello!)");
+
+    }
+
+}
